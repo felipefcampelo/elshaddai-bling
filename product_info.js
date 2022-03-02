5b42e14b1b332f336255079490af45764d35cf04
Felipe Campelo <jobs@felipecampelo.com.br>
2022-03-02 19:12:58 -0300

    Andamento do código de buscar dados do produto

--- product_info.js
+++ product_info.js
@@ -9,20 +9,20 @@
 	// Create the return object
 	var obj = {};
 
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
+	// if (xml.nodeType == 1) { // element
+		// if (xml.attributes.length > 0) {
 		    for (var j = 0; j < xml.attributes.length; j++) {
                 var attribute = xml.attributes.item(j);
 
 				if (attribute.nodeName == 't') {
-                    obj[attribute.nodeName] = attribute.nodeValue;
+                    obj[attribute.nodeValueame] = xml.nodeValue;
                 }
 			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
+		// }
+	// }
+    // else if (xml.nodeType == 3) { // text
+	// 	obj = xml.nodeValue;
+	// }
 
 	// do children
 	if (xml.hasChildNodes()) {
