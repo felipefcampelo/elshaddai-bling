7137ff133ffce0b02fb34624ca62f9aadc5c0ae6
Felipe Campelo <jobs@felipecampelo.com.br>
2022-03-02 18:57:51 -0300

    Andamento do código de buscar dados do produto

--- product_info.js
+++ product_info.js
@@ -56,11 +56,11 @@ function getProdutoData(codigoProduto) {
         url: "https://www.bling.com.br/services/produtos.server.php?f=obterProduto",
         method: "POST",
         headers: {
-            "Origin": "https://www.bling.com.br",
-            "Referer": "https://www.bling.com.br/produtos.php",
-            "session-token": $("#sessid").val(),
-            "User-Agent": "navigator.userAgent",
-            "Cookie": document.cookie
+            "session-token": $("#sessid").val()
+            // "Origin": "https://www.bling.com.br",
+            // "Referer": "https://www.bling.com.br/produtos.php",
+            // "User-Agent": "navigator.userAgent",
+            // "Cookie": document.cookie
         },
         data: {
         xajax: "obterProduto",
