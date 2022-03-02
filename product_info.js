e4474ace53aceb7cb46362de4f8520625a5eeb75
Felipe Campelo <jobs@felipecampelo.com.br>
2022-03-02 18:56:17 -0300

    Andamento do código de buscar dados do produto

--- product_info.js
+++ product_info.js
@@ -2,6 +2,51 @@
 // (Estou usando JQuery que já está inserido no código nativo do Bling)
 
 /**
+ * Converte XML em JSON
+ * @param {*} 
+ */
+ function xmlToJson(xml) {
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		    obj["@attributes"] = {};
+
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+
+	return obj;
+};
+
+/**
  * Busca os dados do produto em uma requisição de imitação do modo como o Blig requisita
  * os dados de um produto
  * @param {string} codigoProduto 
@@ -22,7 +67,7 @@ function getProdutoData(codigoProduto) {
             "xajaxargs[]": codigoProduto
         },
         success: function(response) {
-            console.log(response); // xml
+            console.log(xmlToJson(response)); // Converting XML to JSON
         }
     });
 
